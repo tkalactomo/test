a71b59ccac04a02094b2b55232e5ae9e76bbae0f
tomislav <tkalactomo@orka.hr>
2013-02-28 13:17:28 +0100

    // Promjena 1 - lokalno

--- Test/src/A.java
+++ Test/src/A.java
@@ -1,10 +1,3 @@
 public class A {
-
-	// Promjena za rebase conflict 5 - remote zadnji - REBASE MERGE -// Promjena za rebase conflict 1 - lokalno // Promjena za rebase conflict 1 - lokalno
-
-	//Promjena za rebase conflict 5 - remote zadnji - REBASE MERGE // Promjena za rebase conflict 2 - lokalno // Promjena za rebase conflict 2 - lokalno
-
-	//Promjena za rebase conflict 5 - remote zadnji - REBASE MERGE - // Promjena za rebase conflict 4 - lokalno // Promjena za rebase conflict 4 - lokalno
-
-	//Promjena za rebase conflict 5 - remote zadnji - REBASE MERGE - // Promjena za rebase conflict 5 - lokalno zadnji // Promjena za rebase conflict 5 - lokalno zadnji
+	// Promjena 1 - lokalno
 }
